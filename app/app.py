f582e0bbf0773f17557790c5d9127aee154004fc
DudderThugger <adam.szelestey2002@gmail.com>
2023-02-27 17:18:53 +0100

    monitor module added, error message corrected

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 from scrolllabel import ScrollLabel
+from monitor_layout import MonitorWidget
 import sys
 import serial
 from datetime import datetime
@@ -16,7 +17,7 @@ class MainWindow(QMainWindow):
         self.main_layout = QStackedLayout()
 
         self.menu_init()
-        self.monitor_init()
+        self.monitor_view_widget = MonitorWidget(self)
         #self.send_msg_init()
 
         self.main_layout.addWidget(self.menu_view_widget)
@@ -48,130 +49,11 @@ class MainWindow(QMainWindow):
         self.menu_view_widget = QWidget()
         self.menu_view_widget.setLayout(menu_layout)
 
-    def monitor_init(self):
-        monitor_layout = QVBoxLayout()
-
-        # Setup labels and buttons
-
-        label_monitor = QLabel("Monitor view")
-        label_basic_information = QLabel("Basic description in the future")
-        label_port = QLabel("Port: ")
-        self.lineedit_port = QLineEdit()
-        btn_port = QPushButton("Show port")
-        btn_port.clicked.connect(self.port_number_button)
-
-        # Initialization of the label showcasing the messages
-        self.received_messages = "Recieved messages:\n\n"
-        self.label_received_messages = QLabel(self.received_messages)
-        self.label_received_messages.setWordWrap(True)
-        self.label_received_messages.setMinimumHeight(600)
-        self.label_received_messages.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        
-        # Initialize the scrollable label
-        self.scrolllabel = ScrollLabel(self)
-        self.monitor_running = False
-        self.scrolllabel.setText(self.received_messages)
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.monitoring)
-        self.timer.start(1000)
-
-        # Initialize the "back" button
-        self.btn_back = QPushButton("Back")
-        self.btn_back.clicked.connect(self.back_pushed)
-
-        # Initialize the error label
-        self.label_error = QLabel()
-        self.label_error.setStyleSheet("background-color: red; border: 1px solid black;")
-        self.label_error.setHidden(True)
-
-        monitor_layout.addWidget(label_monitor)
-        monitor_layout.addWidget(label_basic_information)
-        monitor_layout.addWidget(label_port)
-        monitor_layout.addWidget(self.lineedit_port)
-        monitor_layout.addWidget(btn_port)
-        monitor_layout.addWidget(self.scrolllabel)
-        monitor_layout.addWidget(self.btn_back)
-        monitor_layout.addWidget(self.label_error)
-
-        #setup the widget of the monitor view
-        self.monitor_view_widget = QWidget()
-        self.monitor_view_widget.setLayout(monitor_layout)
-
-    def message_reader(self):
-        msg = self.ser.readline().decode('utf-8').split()
-        extended = msg[0][0] == "1"
-        rtr = msg[0][1] == "1"
-        id = msg[1]
-        len = msg[2]
-        ret = ""
-        
-        if msg == "400":
-            ret += "Starting of the CAN bus failed!"
-        else:
-            ret += "Recieved "
-            if extended:
-                ret += "extended "
-            if rtr:
-                ret += "RTR "
-            
-            ret += f"packet with the id 0x{id}"
-
-            if rtr:
-                ret += f" and requested length {len}"
-            else:
-                can_msg = msg[3]
-                ret += f" and length {len}:\n {can_msg}\n\n"
-
-        return ret
-
-    def port_number_button(self):
-        try:
-            usb = self.lineedit_port.text()
-            self.ser = serial.Serial(usb, 115200)
-            self.ser.write('monitor'.encode('utf-8'))
-            self.monitor_running = True
-        except serial.SerialException:
-            self.write_error("Invalid port added!")
-
-
-    def write_error(self, message):
-        self.label_error.setText(message)
-        self.label_error.show()
-
-
     def monitor_pushed(self):
         self.main_layout.setCurrentIndex(1)
-        
-    def monitoring(self):
-        if(self.monitor_running == True):
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            new_message = current_time + ' ' + self.message_reader() + '\n'
-            self.received_messages += new_message
-            self.scrolllabel.setText(self.received_messages)
 
     def send_msg_pushed(self):
         print()
-    
-    def back_pushed(self):
-        self.main_layout.setCurrentIndex(0)
-        self.received_messages = "Recieved messages:\n\n"
-        self.scrolllabel.setText(self.received_messages)
-        try:
-            self.ser.write('exit'.encode('utf-8'))
-        except:
-            pass
-        self.monitor_running = False
-
-    def UiComponents(self): 
-        # creating scroll label
-        label = ScrollLabel(self)
- 
-        # setting text to the label
-        label.setText(self.received_messages)
- 
-        # setting geometry
-        label.setGeometry(100, 100, 200, 80)
 
 app = QApplication(sys.argv)
 
